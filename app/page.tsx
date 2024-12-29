de4557483e61282d3f7fb84235f87190b69bf729
GiwrgosGlinias <gergegl1999@gmail.com>
2024-12-29 15:13:08 +0200

    Tailwind CSS Setup & Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import React from "react";
+
 const Home = () => {
-  return <h1 className="text-red-500">Home</h1>;
+  return (
+    <h1 className="text-light-500 text-2xl">
+      welcome to nextjs 15 welcome to nextjs 15 welcome to nextjs 15
+    </h1>
+  );
 };
 
 export default Home;
